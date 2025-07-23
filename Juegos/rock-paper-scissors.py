496f6881e1ac3c0313cc1b98969af3f897ed34e0
Facundo Feretti <facundoferetti@gmail.com>
2025-07-23 20:14:39 -0300

    realizo contador

--- Juegos/rock-paper-scissors.py
+++ Juegos/rock-paper-scissors.py
@@ -6,7 +6,7 @@ eleccion2 = input(f'Hola, {jugador2} ¿Que eliges? ¿Piedra, Papel o tijeras? ')
 
 condicion1 = eleccion1 == 'piedra' and eleccion2 == 'tijeras' 
 condicion2 = eleccion1 == 'papel' and eleccion2 == 'piedra' 
-condicion3 = eleccion1 == 'tijera' and eleccion2 == 'papel' 
+condicion3 = eleccion1 == 'tijeras' and eleccion2 == 'papel' 
 
 if eleccion1 == eleccion2:
     print('Empate!')
